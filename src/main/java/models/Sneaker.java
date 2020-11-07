7b677ed2ecd3f1951e2859ade8b4a75a5df60b95
buzz <buzz@zipcoders-MacBook-Pro.local>
2020-11-06 23:59:05 -0500

    added test to sneaker

--- src/main/java/models/Sneaker.java
+++ src/main/java/models/Sneaker.java
@@ -1,4 +1,22 @@
 package models;
 
 public class Sneaker {
+
+        private int id;
+        private String name;
+        private String brand;
+        private String sport;
+        private int size;
+        private int qty;
+        private float price;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
 }
